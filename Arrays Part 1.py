486b6586580665c1fca7af7b5f35207c7c2fb257
Mark Tenorio <mtenorio3@gatech.edu>
2018-05-29 11:31:14 -1000

    Initial commit

---
+++ Arrays Part 1.py
@@ -0,0 +1,10 @@
+#### Arrays Part 1 ####
+
+#### Determining if an element is in an array with n elements using LINEARY search ####
+
+# Best case: 1
+# worst case, n comparisons
+# average case, (n+1)/2
+
+# BINARY search: start from the middle of the array
+# log2(N) + 1
